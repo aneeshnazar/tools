18f9755fe3645ce6236953a54481debfd030d600
aneeshnazar <aneesh_naz@yahoo.com>
2018-01-27 19:47:56 -0800

    view_xpm

--- view_xpm.py
+++ view_xpm.py
@@ -4,32 +4,46 @@ import sys
 from PIL import Image
 #import matplotlib.pyplot as plt
 import numpy as np
+import re
+
+fline = re.compile(r"(static.*)")
+lline = re.compile(r"};")
+comment = re.compile(r"^\/\*.*\*\/")
+info = re.compile(r"\"\d+\s+\d+\s+\d+\s+\d+\s*\"")
+colorl = re.compile(r"\"(.)\s+c\s+#([0-9A-Fa-f]{6})\"")
 
 def getXPM(arg):
     ret = []
     col_arr = {}
+    i = 0;
     with open(arg, 'r') as xpm:
-        line = xpm.readline().strip().strip('"')
-        width, height, colors, chars = [int(x) for x in line.split(' ')]
-        for _ in range(colors):
-            line = xpm.readline().strip().strip('"')
-            key, _, color = [x for x in line.split(' ')]
-            col_arr[key] = color
-        for x in range(width):
-            arr = []
-            line = xpm.readline().strip().strip('"')
-            for y in range(height):
-                arr += [[int(col_arr[line[y]][1:3], 16), int(col_arr[line[y]][3:5], 16), int(col_arr[line[y]][5:7], 16)]]
-            ret += [arr]
+        line = xpm.readline()
+        for line in xpm.readlines():
+            line = line.strip()
+            if comment.match(line) or fline.match(line) or lline.match(line):
+                continue
+            elif info.match(line):
+                line = line.strip(',').strip('"')
+                width, height, colors, chars = [int(x) for x in line.split()]
+            elif colorl.match(line):
+                l = colorl.match(line)
+                key, color = l.group(1), l.group(2)
+                col_arr[key] = color
+            else:
+                arr = []
+                line = line.strip('"')
+                for y in range(height):
+                    arr += [[int(col_arr[line[y]][0:2], 16), int(col_arr[line[y]][2:4], 16), int(col_arr[line[y]][4:6], 16)]]
+                ret += [arr]
     return (ret)
 
 def main():
-    arr = getXPM(sys.argv[1])
-    im = Image.fromarray(np.array(getXPM(sys.argv[1]), dtype=np.uint8), 'RGB')
-    #im = np.array(getXPM(sys.argv[1]))
-    #plt.imsave("tmp.jpg",im)
-    #print(im)
-    im.show()
+    if (len(sys.argv) == 2):
+        arr = getXPM(sys.argv[1])
+        im = Image.fromarray(np.array(arr, dtype=np.uint8), 'RGB')
+        im.show()
+    else :
+        print("Usage: ./view_xpm.py [.xpm image]")
 
 if __name__ == '__main__':
     main()
