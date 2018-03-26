6d50328c1e474e978dfaf7b10c8a284bbebd8e24
aneeshnazar <aneesh_naz@yahoo.com>
2018-03-26 13:38:04 -0700

    Iterative fibonacci

---
+++ IterativeFibonacci.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def fibonacci(arg):
+    a = [0, 1, 1];
+    i = 2;
+    if (arg <= 0):
+        return (0);
+    while (i < arg):
+        a[0] = a[1]
+        a[1] = a[2]
+        a[2] = a[1] + a[0]
+        i += 1;
+    return (a[2])
+
+def main():
+    for i in range(200):
+        print (fibonacci(i));
+
+if __name__ == '__main__':
+    main()
